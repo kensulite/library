d66b2dac2535b8820fcf3d320c4fb23efbcf99a9
Jean Alvarez <225872103+kensulite@users.noreply.github.com>
2025-08-13 11:50:51 -0500

    Add dummy books

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 const library = [];
 
+const dummyBooks = [
+    {
+        title: "Book A",
+        author: "Author A",
+        pages: 500,
+        isRead: true
+    },
+    {
+        title: "Book B",
+        author: "Author B",
+        pages: 400,
+        isRead: false
+    },
+    {
+        title: "Book C",
+        author: "Author C",
+        pages: 300,
+        isRead: true
+    }
+];
+
+const addDummyBooks = () => {
+    for (const book of dummyBooks) {
+        addBookToLibrary(book.title, book.author, book.pages, book.isRead);
+    }
+}
+
+const init = () => {
+    addDummyBooks();
+}
+
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -11,3 +42,5 @@ function Book(title, author, pages, isRead) {
 const addBookToLibrary = (title, author, pages, isRead) => {
     library.push(new Book(title, author, pages, isRead));
 }
+
+init();
